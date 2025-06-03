e3f2e06b7e078c9751fc8f513eeebabec5071ad1
Fbeunder <frank@lynxx.eu>
2025-06-03 16:41:23 +0200

    Add hello_world.py - Basic Python script that prints Hello World

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python3
-"""
-Hello World applicatie
-Dit is een eenvoudig Python script dat "Hello World" print.
-"""
-
-def main():
-    """Hoofdfunctie die Hello World print"""
-    print("Hello World")
-
-if __name__ == "__main__":
-    main()
+print("Hello World")
